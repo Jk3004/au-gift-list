2f80a3fc31c1b9ba9f540debacbc108443ed2c8a
jk3004 <jayakrishnansv04@gmail.com>
2023-07-23 16:41:43 +0530

    :sparkles: [client]: Generate Proof

--- client/index.js
+++ client/index.js
@@ -5,13 +5,31 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-    // TODO: how do we prove to the server we're on the nice list? 
+    const proofName = process.argv[2];
+    const checkName = process.argv[3];
 
-    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-        // TODO: add request body parameters here!
-    });
+    console.log(proofName);
+    console.log(checkName);
+    if (proofName == null || checkName == null) {
+        console.log('Usage: node client/index.js "name-to-proof" "name-to-check"');
+        process.exit();
+    }
 
-    console.log({ gift });
+    const index = niceList.findIndex(name => name === proofName);
+    console.log(index);
+    if (index < 0) {
+        console.log("Please enter a valid name to build proof");
+        process.exit();
+    }
+
+    const merkleTree = new MerkleTree(niceList);
+    const proof = merkleTree.getProof(index);
+
+    // const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    //     // TODO: add request body parameters here!
+    // });
+
+    // console.log({ gift });
 }
 
 main();
